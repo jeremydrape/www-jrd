b5a2293aac87817ea8736487319ff47cba06de62
Rohan Drape <rd@slavepianos.org>
2012-04-06 12:55:31 +1000

    +theindex(theindex)
    Ignore-this: bac4a202880f060ed68e7e14676c978f
    
    darcs-hash:20120406025531-2b513-911dbcd4b2e01fa8377551c4faee867872730db0

--- theindex/config.php
+++ theindex/config.php
@@ -2,7 +2,7 @@
 
 $indx['db']             = 'jrd_theindex';
 $indx['user']           = 'jrd';
-$indx['pass']           = 'atdsiuytasdt';
+$indx['pass']           = 'theindex';
 $indx['host']           = 'sql.jeremydrape.com';
 $indx['sql']            = 'mysql';
 
