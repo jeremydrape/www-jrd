984957e18dcce846abae78c4099d8cb2fbe1a9b1
Rohan Drape <rd@slavepianos.org>
2012-04-06 11:55:02 +1000

    +theindex(config/style)
    Ignore-this: bf552683bde9bbf447144fa562019370
    
    darcs-hash:20120406015502-2b513-b13963beeb042315bf877734e24a61f60b893eee

---
+++ theindex/config.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db']             = 'jrd_theindex';
+$indx['user']           = 'jrd';
+$indx['pass']           = 'atdsiuytasdt';
+$indx['host']           = 'sql.jeremydrape.com';
+$indx['sql']            = 'mysql';
+
+?>
