83b5277ebbf72c664d7c0d004050bd0356d9e5b9
jeremy drape <jeremy.drape@gmail.com>
2013-06-18 19:28:45 +1000

    +titles(jrd)
    Ignore-this: bfdb5760e63686104842109939cc9bb7
    
    darcs-hash:20130618092845-607b5-4ceb007453655e844206ff4be1d5e4552dc34c2c

--- data/hs/images.hs
+++ data/hs/images.hs
@@ -1,4 +1,4 @@
-[("57_weight-2","untitled")
-,("97870034","untitled")
-,("img_4385","untitled")
+[("57_weight-2","The Weight of the World, 2011")
+,("97870034","Sanja Pahoki in her studio, 2008")
+,("img_4385","Screen #2, 2012")
 ,("img_5066","untitled")]
