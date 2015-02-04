bc5dc8f1c3e31168b04aea0bc45229914381d1bf
jrd <contact@jeremydrape.com>
2015-02-03 17:45:34 -0800

    jrd.edits(DD_{6,7})

--- data/hs/images.hs
+++ data/hs/images.hs
@@ -4,4 +4,6 @@
  ,("Inside4","Untitled, 2014")
  ,("Inside5","Untitled, 2014")
  ,("Inside6","Untitled, 2014")]
-,[("DD_4","Untitled, 2014")]]
+,[("DD_4","Split Stairs, 2014")
+,("DD_7","Mile End, 2014")
+,("DD_6","Fernandez & Wells, 2014")]]
