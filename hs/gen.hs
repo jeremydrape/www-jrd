f77bc275c04f7661bdcedacae6972dfc09696a3b
rohan <rd@slavepianos.org>
2021-10-15 10:35:03 +1100

    ~css/rand

--- hs/gen.hs
+++ hs/gen.hs
@@ -6,13 +6,13 @@ import qualified Sound.SC3.Lang.Random.Gen as L {- hsc3-lang -}
 
 import qualified JRD as J {- ut/www-jrd -}
 
--- > take 4 img_offsets
-img_offsets :: [[Int]]
-img_offsets =
-    let t = L.s_rrand 0 300 (mkStdGen 246873)
-        r = L.s_rrand 0 150 (mkStdGen 174027)
-        b = L.s_rrand 0 300 (mkStdGen 380984)
-        l = L.s_rrand 0 150 (mkStdGen 023664)
+-- > take 4 $ img_offsets (400, 200)
+img_offsets :: (Int, Int) -> [[Int]]
+img_offsets (h, w) =
+    let t = L.s_rrand 0 h (mkStdGen 246873)
+        r = L.s_rrand 0 w (mkStdGen 174027)
+        b = L.s_rrand 0 h (mkStdGen 380984)
+        l = L.s_rrand 0 w (mkStdGen 023664)
     in transpose [t,r,b,l]
 
 mk_padding :: [Int] -> String
@@ -25,4 +25,4 @@ img_id_rand_css :: Int -> [Int] -> String
 img_id_rand_css n p = printf "div.%s {%s}" (J.img_id n) (mk_padding p)
 
 gen :: IO ()
-gen = putStrLn $ unlines $ zipWith img_id_rand_css [0..200] img_offsets
+gen = putStrLn $ unlines $ zipWith img_id_rand_css [0..200] (img_offsets (400, 200))
